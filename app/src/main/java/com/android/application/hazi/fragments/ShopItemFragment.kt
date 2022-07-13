748918ea6b79c0cdb63a31f97e31317896b74f06
fptitsyn <fedya.ptitsyn@mail.ru>
2022-07-14 01:29:58 +0300

    Added rewards for task; added function to add coins to user; added views to display user's coins; handled the situation when user already has an item

--- app/src/main/java/com/android/application/hazi/fragments/ShopItemFragment.kt
+++ app/src/main/java/com/android/application/hazi/fragments/ShopItemFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.android.application.hazi.R
 import com.android.application.hazi.databinding.FragmentShopItemScreenBinding
@@ -15,7 +16,6 @@ import com.android.application.hazi.models.ShopItem
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
@@ -32,8 +32,10 @@ class ShopItemFragment : Fragment() {
 
     private var coins = 0
 
+    private var canBuyItem = true
+
     companion object {
-        val TAG = ShopItemFragment::class.java.simpleName
+        val TAG: String = ShopItemFragment::class.java.simpleName
     }
 
     override fun onCreateView(
@@ -78,8 +80,13 @@ class ShopItemFragment : Fragment() {
             binding.shopItemImageView.setImageResource(R.drawable.ic_shop)
         }
 
+        updateUiFromDatabase(shopItemName, shopItemPrice)
+    }
+
+    private fun updateUiFromDatabase(shopItemName: String, shopItemPrice: String) {
         // Get user coins
-        database = FirebaseDatabase.getInstance("https://hazi-8190a-default-rtdb.europe-west1.firebasedatabase.app/")
+        database =
+            FirebaseDatabase.getInstance("https://hazi-8190a-default-rtdb.europe-west1.firebasedatabase.app/")
 
         currentUserId = Firebase.auth.currentUser?.uid
         lifecycleScope.launch {
@@ -87,16 +94,44 @@ class ShopItemFragment : Fragment() {
                 .orderByChild("id").equalTo(currentUserId).get()
                 .await().children.first().ref
 
+            val userShopItemsRef = userDatabaseReference.child("shopItems")
+
+            userShopItemsRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val shopItems: HashMap<*, HashMap<String, *>> =
+                        snapshot.value as HashMap<String, HashMap<String, *>>
+
+                    for (shopItem in shopItems.values) {
+                        Log.d(TAG, shopItem.toString())
+                        val userShopItemName = shopItem.get("name")
+                        if (shopItemName == userShopItemName) {
+                            binding.buyShopItemButton.isEnabled = false
+                            canBuyItem = false
+                            Toast.makeText(
+                                requireContext(),
+                                "Item already owned",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.d(TAG, "Database error occurred: $error")
+                }
+            })
+
             val userCoins = userDatabaseReference.child("coins")
 
             userCoins.addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     coins = snapshot.value.toString().toInt()
-                    Log.d(TAG, coins.toString())
 
-                    // Make button disabled if user doesn't have enough coins
-                    binding.buyShopItemButton.isEnabled = coins >= shopItemPrice.toInt()
-                    Toast.makeText(requireContext(), "$coins", Toast.LENGTH_SHORT).show()
+                    // Make button disabled if user doesn't have enough coins and display the coins amount
+                    if (canBuyItem) {
+                        binding.buyShopItemButton.isEnabled = coins >= shopItemPrice.toInt()
+                    }
+                    binding.userCoins.text = "$coins coins"
                 }
 
                 override fun onCancelled(error: DatabaseError) {
@@ -124,5 +159,7 @@ class ShopItemFragment : Fragment() {
             "$shopItemName has been bought successfully",
             Toast.LENGTH_SHORT
         ).show()
+
+        findNavController().navigateUp()
     }
 }
